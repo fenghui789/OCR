43e3418911c4e565471839102739ec6c180c9792
hui <55653809+fenghui789@users.noreply.github.com>
2019-10-04 17:20:00 +0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,62 @@
+# -*- coding: UTF-8 -*-
+import http.server
+import json
+from ocr import OCRNeuralNetwork
+import numpy as np
+
+HOST_NAME = 'localhost'
+PORT_NUMBER = 8001
+HIDDEN_NODE_COUNT = 15
+
+#将数据样本和标签加载到矩阵中 
+data_matrix = np.loadtxt(open('data.csv', 'rb'), delimiter = ',')
+data_labels = np.loadtxt(open('dataLabels.csv', 'rb'))
+
+#从numpy ndarrays转换为python列表
+data_matrix = data_matrix.tolist()
+data_labels = data_labels.tolist()
+
+#如果神经网络文件不存在，请使用所有5000个现有数据样本对其进行训练。
+#根据从Neuro_network_design.py收集的数据，最佳值为15
+#用于隐藏节点
+nn = OCRNeuralNetwork(HIDDEN_NODE_COUNT, data_matrix, data_labels, list(range(5000)));
+
+class JSONHandler(http.server.BaseHTTPRequestHandler):
+    def do_POST(s):
+        response_code = 200
+        response = ""
+        var_len = int(s.headers.get('Content-Length'))
+        content = s.rfile.read(var_len);
+        payload = json.loads(content);
+
+        if payload.get('train'):
+            nn.train(payload['trainArray'])
+            nn.save()
+        elif payload.get('predict'):
+            try:
+                response = {"type":"test", "result":nn.predict(str(payload['image']))}
+            except:
+                response_code = 500
+        else:
+            response_code = 400
+
+        s.send_response(response_code)
+        s.send_header("Content-type", "application/json")
+        s.send_header("Access-Control-Allow-Origin", "*")
+        s.end_headers()
+        if response:
+            s.wfile.write(json.dumps(response))
+        return
+
+if __name__ == '__main__':
+    server_class = http.server.HTTPServer;
+    httpd = server_class((HOST_NAME, PORT_NUMBER), JSONHandler)
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    else:
+        print ("Unexpected server exception occurred.")
+    finally:
+        httpd.server_close()
